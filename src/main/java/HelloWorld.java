e9838d8b02d240df7306f58a3cf6bf872eec3ca0
= <sn76@st-andrews.ac.uk>
2020-10-31 12:15:45 +0000

    add simple jcodec code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 import org.jcodec.api.FrameGrab;
 import org.jcodec.api.JCodecException;
 import org.jcodec.common.io.NIOUtils;
+import org.jcodec.containers.mp4.demuxer.MP4Demuxer;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -10,8 +11,12 @@ public class HelloWorld
     
     public static void main(String[] args) throws IOException, JCodecException
     {
-        var frameGrab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(Paths.get(args[0]).toFile()));
-        var info = frameGrab.getMediaInfo();
-        System.out.println(info.getDim().getHeight() + ", " + info.getDim().getWidth());
+        var channel = NIOUtils.readableChannel(Paths.get(args[0]).toFile());
+        var frameGrab = FrameGrab.createFrameGrab(channel);
+        var demuxer = MP4Demuxer.createMP4Demuxer(channel);
+        var track = demuxer.getVideoTrack();
+        var info = track.getMeta();
+        var size = info.getVideoCodecMeta().getSize();
+        System.out.println(size.getWidth() + "x" + size.getHeight() + ", " + info.getTotalFrames() + " frames @ " + info.getTotalFrames() / info.getTotalDuration() + "fps");
     }
 }
