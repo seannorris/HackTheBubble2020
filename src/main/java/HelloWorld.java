6ee9f9d0535ea1752d56de88dac56ec98c34ceb2
AgnesLL <li62232954@gmail.com>
2020-10-31 12:40:48 +0000

    read image'

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,19 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class HelloWorld
 {
     public static void main(String[] args)
     {
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File("pic.jpg"));
-        } catch (IOException e) {
+        try{
+            BufferedImage img = null;
+            // TODO: change relative path
+            img = ImageIO.read(new File("C:\\Users\\user\\Documents\\all\\programming\\bubble\\HackTheBubble2020\\src\\main\\java\\pic.jpg"));
+        } catch (Exception e) {
+            System.out.print(e);
         }
+        
     }
 }
