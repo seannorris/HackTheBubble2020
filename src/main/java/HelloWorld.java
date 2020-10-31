ef3eaa747bb83f730e1b527c45bf304c16e7fbc4
= <sn76@st-andrews.ac.uk>
2020-10-31 12:06:07 +0000

    add simple jcodec code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,17 @@
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.io.NIOUtils;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class HelloWorld
 {
     
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, JCodecException
     {
-        System.out.println("Hello World!");
+        var frameGrab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(Paths.get(args[0]).toFile()));
+        var info = frameGrab.getMediaInfo();
+        System.out.println(info.getDim().getHeight() + ", " + info.getDim().getWidth());
     }
 }
